453877b9f7e92d5b767886e536518d4bf3ec165f
blockvished <jcmvishael@gmail.com>
2025-08-23 15:11:16 +0000

    cursor is moving now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+  // console.log(mousePosition)
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      // console.log(e)
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    window.addEventListener('mousemove', mouseMove)
+
+    return () => {
+      window.removeEventListener('mousemove', mouseMove)
+    }
+  }, [])
+
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+    }
+  }
+
   return (
     <div className='App'>
       <h1 className='title'>Hello Victor</h1>
-      <div className='cursor'/>
+      <motion.div
+        className='cursor'
+        variants={variants}
+        animate="default" />
     </div>
   )
 }
