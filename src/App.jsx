0422593e0d77629dfb11cb040a5fc409f4ff7fc2
blockvished <jcmvishael@gmail.com>
2025-08-23 15:03:02 +0000

    cursor is visible but not movable

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import './App.css'
+
 function App() {
   
   return (
     <div className='App'>
       <h1 className='title'>Hello Victor</h1>
+      <div className='cursor'/>
     </div>
   )
 }
