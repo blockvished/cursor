a6f2ad11b57f79d4f8fbe8208e99362f43086a8f
blockvished <jcmvishael@gmail.com>
2025-08-23 16:13:40 +0000

    movement with cursor using gsap

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,7 +1,23 @@
+import gsap from "gsap"
+import { useEffect } from "react"
+
 function Cursor() {
 
+    const handleMouseMove = (event) => {
+        const { clientX, clientY } = event
+        gsap.to("#cursor", {
+            x: clientX-20/2,
+            y: clientY-20/2,
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousemove", handleMouseMove)
+        return () => { window.removeEventListener("mousemove", handleMouseMove) }
+    })
+
     return (
-        <div className='absolute top-0 left-0 h-[20px] w-[20px] bg-white rounded-full z-10' />
+        <div id="cursor" className='absolute top-0 left-0 h-[20px] w-[20px] bg-white rounded-full z-10' />
     )
 }
 
