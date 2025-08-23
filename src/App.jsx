44bad3f0495d0d75980d6ff67718120d47f62472
blockvished <jcmvishael@gmail.com>
2025-08-23 16:06:21 +0000

    showing the unmoving cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Cursor from './components/Cursor'
 function App() {
 
   return (
-    <div className='w-full h-screen bg-zinc-900 text-white flex justify-center items-center'>Hello Victor
+    <div className='w-full h-screen bg-zinc-900 text-white flex justify-center items-center'>
+      <Cursor />
+      Hello Victor
     </div>
   )
 }
---
+++ src/components/Cursor.jsx
@@ -0,0 +1,8 @@
+function Cursor() {
+
+    return (
+        <div className='absolute top-0 left-0 h-[20px] w-[20px] bg-white rounded-full z-10' />
+    )
+}
+
+export default Cursor
