ee57f49b94d15a40292bbf15153e63b4ac25cdb7
blockvished <jcmvishael@gmail.com>
2025-08-24 07:22:23 +0000

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
-import Cursor from './components/Cursor'
+import "./App.css";
+import CustomCursor from "./components/Cursor";
 
 function App() {
   return (
-    <div className=''>
-      <Cursor />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <CustomCursor />
+
+      <h1 className="text-5xl font-bold underline mb-8 text-center">
+        Hello Victor!
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-4">
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Default
+        </button>
+        <button
+          type="button"
+          className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        >
+          Alternative
+        </button>
+        <button
+          type="button"
+          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          Dark
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
